d400851a283a9d35f980f87f8f9b0337efff85d5
anru <ctx2002@gmail.com>
2015-03-27 22:32:32 +1300

    removed some comments

--- src/SQL/Token.php
+++ src/SQL/Token.php
@@ -1,17 +1,7 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 namespace SQL;
 
-/**
- *
- * @author anru
- */
+
 interface Token {
     public function lexeme();
     public function match($input,$offset);
